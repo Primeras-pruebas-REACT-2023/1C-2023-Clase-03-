e2a47b4f8ef730529862e5dba9f2e42238cedb66
Walter Mancuso <waltermancuso@abc.gob.ar>
2023-07-02 01:10:30 -0300

    Ultima version con tarea resuelta :-)

--- JS/app.js
+++ JS/app.js
@@ -4,3 +4,7 @@ let b = 500;
 console.log(a+b);
 let myDom = document;
 console.log(myDom);
+
+let myapp = document.getElementById("mi primera app");
+let Nombre = prompt ("Como te llamas?");
+myapp.innerHTML = "Hola " + Nombre;
